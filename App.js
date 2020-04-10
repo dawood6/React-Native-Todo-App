68625886bffd756379a35a57b7f772ae8bd2c4c9
dawood6 <dawoodsadiq171@gmail.com>
2020-04-10 22:50:30 +0500

    Feat: Fixed & Updated Code

--- App.js
+++ App.js
@@ -1,79 +1,83 @@
-import React, { Fragment, Component } from 'react';
-import { StyleSheet, Text, View, TextInput, Dimensions, Button } from 'react-native';
-
-
+import React, { Fragment, Component } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Dimensions,
+  Button,
+  FlatList,
+} from "react-native";
+import SpinnerButton from "react-native-spinner-button";
 
 const { width } = Dimensions.get("window");
 
-export default class App extends Component{
-
+export default class App extends Component {
   state = {
     Todo: "",
     Todos: [],
-  }
+    defaultLoading: false
+  };
+
   handleChange = (event) => {
     this.setState({ Todo: event.nativeEvent.text });
   };
-  addTodo = event => {
+  addTodo = (event) => {
     const { Todo, Todos } = this.state;
-    if (Todo === "") return alert('error');
+    if (Todo === "") return alert("Error: No Input");
     const merged = [...Todos, Todo];
     this.setState({ Todos: merged, Todo: "" });
-  }
-  delete = (removedTodo) => {
-    const { Todos } = this.state
-    const filteredTodos = Todos.filter(item => item !== removedTodo)
-    this.setState({ Todos: filteredTodos })
-    alert('done')
   };
-  render(){
-  return (
-    <Fragment>
-    <View style={styles.container}>
-      <Text style={styles.text}>React Native Todo App</Text>
-    </View>
-    <View style={styles.inputField}>
-    <TextInput
-    placeholder= 'hello'
-    style={styles.input}
-    onChange={this.handleChange}
-    value={this.state.Todo}
-    />
-    <Button  onPress={this.addTodo} title= 'Add' />
-
-    <Text>
-    {this.state.Todos.map(item => (
-              <Text style={{fontSize: 20,}}>
-                {item}{" "}
-                <Text  onPress={() => this.delete(item)} >❌</Text>{" "}
+  delete = (removedTodo) => {
+    const { Todos } = this.state;
+    const filteredTodos = Todos.filter((item) => item !== removedTodo);
+    this.setState({ Todos: filteredTodos });
+    alert("Done");
+  };
+  render() {
+    return (
+      <Fragment>
+        <View style={styles.container}>
+          <Text style={styles.text}>React Native Todo App</Text>
+        </View>
+        <View style={styles.inputField}>
+          <TextInput
+            placeholder="Enter Todo"
+            style={styles.input}
+            onChange={this.handleChange}
+            value={this.state.Todo}
+          />
+          <Button onPress={this.addTodo}>Add Todo</Button>
+          <Text>
+            {this.state.Todos.map((item) => (
+              <Text style={{ fontSize: 20 }}>
+                {item} <Text onPress={() => this.delete(item)}>❌</Text> {"\n"}
               </Text>
             ))}
-            {console.log(this.state.Todos)}
-    </Text>
-    </View>
-    </Fragment>
-  );
-}
+          </Text>
+        </View>
+      </Fragment>
+    );
+  }
 }
 const styles = StyleSheet.create({
   container: {
     flex: 2,
-    // backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
+    alignItems: "center",
+    justifyContent: "flex-start",
   },
   text: {
     fontSize: 35,
     marginTop: 20,
   },
   input: {
-   width,
-   fontSize: 20,
-   textAlign: "center",
+    width,
+    fontSize: 20,
+    textAlign: "center",
   },
   inputField: {
     flex: 5,
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-  }
+    alignItems: "center",
+    justifyContent: "flex-start",
+  },
 });
